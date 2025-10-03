3c5d74b803591bb62fe43d7ba978698c72ce4d1f
Christopher <casmir.c.f@gmail.com>
2025-10-03 16:57:36 +0300

    merge from main

--- core/src/commonMain/kotlin/kevlar/Action0Container.kt
+++
@@ -1,8 +0,0 @@
-@file:JsExport
-@file:Suppress("WRONG_EXPORTED_DECLARATION")
-
-package kevlar
-
-import kotlinx.JsExport
-
-interface Action0Container<out R> : Action0<R>, ActionContainer<() -> R>
--- core/src/commonMain/kotlin/kevlar/Action1Container.kt
+++
@@ -1,8 +0,0 @@
-@file:JsExport
-@file:Suppress("WRONG_EXPORTED_DECLARATION")
-
-package kevlar
-
-import kotlinx.JsExport
-
-interface Action1Container<in I, out R> : Action1<I, R>, ActionContainer<(I) -> R>
